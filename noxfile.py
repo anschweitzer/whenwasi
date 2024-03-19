f11792ab2eb55d6b6ca1dbbe145c61a92b854e87
Andrew Schweitzer <schweitz72@gmail.com>
2024-03-19 12:00:41 -0400

    Added data.py experiments (untested) and updates for vscode

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,6 @@
+# type: ignore
 """Nox sessions."""
+
 import os
 import shlex
 import shutil
@@ -23,14 +25,14 @@ except ImportError:
 
 
 package = "whenwasi"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
+python_versions = ["3.12", "3.11", "3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
-    "safety",
+    # "safety",
     "mypy",
     "tests",
-    "typeguard",
+    # "typeguard",
     "xdoctest",
     "docs-build",
 )
